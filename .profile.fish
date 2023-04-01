00774d4f4b5eb49fb806817d4f049f5e6e69210e
Júlia Biró <juliabiro@users.noreply.github.com>
2023-04-01 21:46:12 +0200

    fix home path

--- .profile.fish
+++ .profile.fish
@@ -1,24 +1,24 @@
 
 # set paths
-#set -gx OMF_PATH "/Users/julia.biro/.local/share/omf"
+#set -gx OMF_PATH "/$HOME/.local/share/omf"
 set -x PKG_CONFIG_PATH /opt/X11/lib/pkgconfig/
 set -x HAXE_STD_PATH /usr/local/lib/haxe-3.1.3
 
 #set -x PATH $PATH $HOME/.rvm/bin # Add RVM to PATH for scripting
 #set -x PATH $PATH $HOME/.emacs.d/ical2org.awk # Add ical2org script
 set -x PATH /usr/local/bin /usr/local/Cellar/go/1.19.3/bin /opt/homebrew/bin $PATH
-set -x PATH $PATH /Users/julia.biro/go/bin/
+set -x PATH $PATH $HOME/go/bin/
 #set -x PATH $PATH /Library/TeX/texbin/
 #set -x PATH $PATH /Library/Frameworks/Python.framework/Versions/2.7/bin/
 set -x PATH $PATH /usr/local/opt/libpq/bin 
 
 set -x PATH $PATH /Applications/
 # expander
-set -x PATH $PATH /Users/julia.biro/go/src/github.com/juliabiro/expander
-set -x EXPANDER_CONF /Users/julia.biro/.kube/expander_conf.json
+set -x PATH $PATH /$HOME/go/src/github.com/juliabiro/expander
+set -x EXPANDER_CONF /$HOME/.kube/expander_conf.json
 
 # kmux
-set -x PATH $PATH /Users/julia.biro/go/src/github.com/juliabiro/kubectl_mux
+set -x PATH $PATH /$HOME/go/src/github.com/juliabiro/kubectl_mux
 
 set -x AWS_CREDENTIAL_FILE $HOME/.aws/credentials
 
