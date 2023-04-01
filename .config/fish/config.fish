0a0a69e374233dcd3bd4914e79fc6cbafd392b6e
Júlia Biró <juliabiro@users.noreply.github.com>
2023-04-01 17:59:50 +0200

    some config

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/juliabiro/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG "/Users/juliabiro/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+source $HOME/.profile.fish
+source $HOME/.alias.fish
+#ssh-add ~/.ssh/id_rsa_*
+
+#if test -e "$HOME/.rvm/scripts/rvm"
+ #   "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+#end
+#rvm --default use 2.1.0
+eval (direnv hook fish)
+
+function fish_title
+  true
+end
