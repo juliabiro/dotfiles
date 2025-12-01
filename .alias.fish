5f22ee811df93014076ab71d02f055386a199811
Julia Biro <9192408+juliabiro@users.noreply.github.com>
2025-12-01 13:33:44 +0100

    test commit

--- .alias.fish
+++ .alias.fish
@@ -4,12 +4,7 @@
 
 alias ll="ls -lah"
 
-# set easyssh_executor "(if-command (ssh-exec-parallel) (if-one-target (ssh-login) (csshx)))"
-# set easyssh_discoverer "(first-matching (comma-separated))"
-# set easyssh_filter "(list (ec2-instance-id us-east-1) (ec2-instance-id us-west-1))"
 
-# alias s="easyssh -e='$easyssh_executor' -d='$easyssh_discoverer' -f='$easyssh_filter'"
-# alias sr='s -l root'
 
 alias gs="git status"
 alias gc="git commit -m"
