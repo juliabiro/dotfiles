961ac2c9905d6f2a6bbeea6ef731e590bb9b2624
Julia Biro <9192408+juliabiro@users.noreply.github.com>
2025-12-11 10:30:52 +0100

    orbstack

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 function fish_title
   true
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
