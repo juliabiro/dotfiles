41468c8e7d1c7ea9bb9d2eb9e373e3389f4264ad
Julia Biro <9192408+juliabiro@users.noreply.github.com>
2026-01-09 14:48:04 +0100

    different context

--- .profile.fish
+++ .profile.fish
@@ -26,7 +26,7 @@ set -x AWS_CREDENTIAL_FILE $HOME/.aws/credentials
 set -x EDITOR emacsclient
 #set -x  python2 /usr/bin/python
 
-kubectl config use-context gke_kombo-staging_europe-north2-a_kombo-cluster-eun2
+kubectl config use-context gke_kombo-staging_europe-west4-a_kombo-cluster-euw4
 
 set -x KUBEBUILDER_ASSETS /usr/local/share/kubebuilder
 
